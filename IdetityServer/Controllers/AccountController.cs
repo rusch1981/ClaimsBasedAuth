ececf7d6cf5a899fdc9b211b99d04289649a5d9f
Adam Rusch <arusch@imagineteam.com>
2018-11-13 08:29:41 -0500

    Removed used AccountContoller methods and imported fullframwork client

--- IdetityServer/Controllers/AccountController.cs
+++ IdetityServer/Controllers/AccountController.cs
@@ -407,38 +407,7 @@ namespace IdentityServer.Controllers
                 return Challenge(AccountOptions.WindowsAuthenticationSchemeName);
             }
         }
-
-        //todo remove?
-        private (User user, string provider, string providerUserId, IEnumerable<Claim> claims) FindUserFromExternalProvider(AuthenticateResult result)
-        {
-            var externalUser = result.Principal;
-
-            // try to determine the unique id of the external user (issued by the provider)
-            // the most common claim type for that are the sub claim and the NameIdentifier
-            // depending on the external provider, some other claim type might be used
-            var userIdClaim = externalUser.FindFirst(JwtClaimTypes.Subject) ??
-                              externalUser.FindFirst(ClaimTypes.NameIdentifier) ??
-                              throw new Exception("Unknown userid");
-
-            // remove the user id claim so we don't include it as an extra claim if/when we provision the user
-            var claims = externalUser.Claims.ToList();
-            claims.Remove(userIdClaim);
-
-            var provider = result.Properties.Items["scheme"];
-            var providerUserId = userIdClaim.Value;
-
-            // find user
-            var user = _users.FindByUsername(userIdClaim.Value);
-
-            return (user, provider, providerUserId, claims);
-        }
-
-        private User AutoProvisionUser(string provider, string providerUserId, IEnumerable<Claim> claims)
-        {
-            var user = _users.AutoProvisionUser(provider, providerUserId, claims.ToList());
-            return user;
-        }
-
+        
         private void ProcessLoginCallbackForOidc(AuthenticateResult externalResult, List<Claim> localClaims, AuthenticationProperties localSignInProps)
         {
             // if the external system sent a session id claim, copy it over
---
+++ MVC_FrameworkClient/MvcIdentityServer/README.md
@@ -0,0 +1,8 @@
+﻿# MVCIdentityServer
+This Client was created based off of MVC Owin Client (Implicit) that can be found 
+[IdentityServer3.Samples.](https://github.com/IdentityServer/IdentityServer3.Samples).
+
+The current configuration is set to work with IdenityServer4Example server using Windows 
+Authentication. No Configuration for Windows Authentication is required via
+the client.  The README in IdenityServer4Example notes the important configuration 
+aspects at a server level.
