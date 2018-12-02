63e241f48f55f13b430bc999b928026d40215cec
rusch <rusch@DESKTOP-PROEALE>
2018-12-02 17:43:53 -0500

    Trying to find a routing solution...

--- FormsAuthFFClient/Startup.cs
+++ FormsAuthFFClient/Startup.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
@@ -21,4 +24,32 @@ namespace FormsAuthFFClient
             });
         }
     }
+
+    public class CustomMiddleware1
+    {
+        Func<IDictionary<string, object>, Task> _next;
+        private readonly IAppBuilder _appBuilder;
+
+        public CustomMiddleware1(Func<IDictionary<string, object>, Task> next, IAppBuilder appBuilder)
+        {
+            _next = next;
+            _appBuilder = appBuilder;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            //No solution in place. Not really sure what to do....  
+            //https://visualstudiomagazine.com/articles/2015/05/01/inject-custom-middleware.aspx
+            new CookieAuthenticationMiddleware(, _appBuilder, new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                CookieSecure = CookieSecureOption.SameAsRequest
+            });
+            //pre
+            await _next.Invoke(environment);
+            //post
+        }
+    }
 }
+
