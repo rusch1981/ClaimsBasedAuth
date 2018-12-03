57bc31a55d8f2d4af7783c3eb991b147a96b6543
rusch <rusch@DESKTOP-PROEALE>
2018-12-03 02:44:53 -0500

    Middleware configured with two auth providers.

--- FormsAuthFFClient/Startup.cs
+++ FormsAuthFFClient/Startup.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web.Helpers;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
+using IdentityServer3.Core;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security;
 
 [assembly: OwinStartup(typeof(FormsAuthFFClient.Startup))]
 
@@ -14,10 +19,11 @@ namespace FormsAuthFFClient
     public class Startup
     {
         public void Configuration(IAppBuilder app)
-        {
-            System.Web.Helpers.AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Sid;
+        {           
 
             app.Use(typeof(CustomMiddleware2), app);
+
+            //System.Web.Helpers.AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Sid;
 
             //app.UseCookieAuthentication(new CookieAuthenticationOptions
             //{
@@ -43,20 +49,73 @@ namespace FormsAuthFFClient
 
             if (isRoute1)
             {
-                var authMiddleware1 = new CookieAuthenticationMiddleware(Next, _appBuilder, new CookieAuthenticationOptions
+                var openIdConnectMiddleware = new OpenIdConnectAuthenticationMiddlewareWrapper(Next, _appBuilder, new OpenIdConnectAuthenticationOptions()
                 {
-                    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                    LoginPath = new PathString("/Account/Login"),
-                    CookieSecure = CookieSecureOption.SameAsRequest
+                    Authority = "http://localhost:5000",
+
+                    ClientId = "mvc2",
+                    ClientSecret = "secret",
+                    RedirectUri = "http://localhost:49820/",
+                    ResponseType = "code id_token",
+
+                    Scope = "openid profile customProfile1 customProfile2",
+                    //this is avoid error with https redirect
+                    RequireHttpsMetadata = false,
+                    SignInAsAuthenticationType = "Cookies",
+                    UseTokenLifetime = false,
+
+
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        SecurityTokenValidated = n =>
+                        {
+                            var id = n.AuthenticationTicket.Identity;
+
+                            // we want to keep first name, last name, subject and roles
+                            var sub = id.FindFirst(IdentityServer3.Core.Constants.ClaimTypes.Subject);
+                            var roles = id.FindFirst(IdentityServer3.Core.Constants.ClaimTypes.Role);
+                            var name = id.FindFirst("name");
+
+
+                            // create new identity and set name and role claim type
+                            var nid = new ClaimsIdentity(
+                                id.AuthenticationType,
+                                IdentityServer3.Core.Constants.ClaimTypes.Name,
+                                IdentityServer3.Core.Constants.ClaimTypes.Role);
+
+                            nid.AddClaim(sub);
+                            nid.AddClaim(roles);
+                            nid.AddClaim(name);
+
+                            n.AuthenticationTicket = new AuthenticationTicket(
+                                nid,
+                                n.AuthenticationTicket.Properties);
+
+                            return Task.FromResult(0);
+                        }
+                    }
                 });
 
+                //Probably need to wrap the above in a class to make Next public
+                var cookieAuthenticationMiddleware = new CookieAuthenticationMiddleware(openIdConnectMiddleware.NextWrapper, _appBuilder, new CookieAuthenticationOptions
+                {
+                    AuthenticationType = "Cookies"
+                });
+
+
+
+                AntiForgeryConfig.UniqueClaimTypeIdentifier = IdentityServer3.Core.Constants.ClaimTypes.Subject;
+                JwtSecurityTokenHandler.DefaultInboundClaimTypeMap = new Dictionary<string, string>();
+
                 context.Response.Write("<h1>PreCustomMiddleware Meesage 1</h1>");
-                await authMiddleware1.Invoke(context);
+                await cookieAuthenticationMiddleware.Invoke(context);
                 context.Response.Write("<h1>PostCustomMiddleware Meesage 1</h1>");
             }
             else
             {
-                var authMiddleware1 = new CookieAuthenticationMiddleware(Next, _appBuilder, new CookieAuthenticationOptions
+                System.Web.Helpers.AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Sid;
+
+                var authMiddleware2 = new CookieAuthenticationMiddleware(Next, _appBuilder, new CookieAuthenticationOptions
                 {
                     AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                     LoginPath = new PathString("/Account/Login"),
@@ -64,9 +123,27 @@ namespace FormsAuthFFClient
                 });
 
                 context.Response.Write("<h1>PreCustomMiddleware Meesage 2</h1>");
-                await authMiddleware1.Invoke(context);
+                await authMiddleware2.Invoke(context);
                 context.Response.Write("<h1>PostCustomMiddleware Meesage 2</h1>");
             }
         }
     }
+
+    public class OpenIdConnectAuthenticationMiddlewareWrapper : OpenIdConnectAuthenticationMiddleware
+    {
+        public OwinMiddleware NextWrapper
+        {
+            get
+            {
+                return Next;
+            }
+        }
+
+        public OpenIdConnectAuthenticationMiddlewareWrapper(OwinMiddleware next, IAppBuilder appBuilder, OpenIdConnectAuthenticationOptions options) 
+            : base(next, appBuilder, options)
+        {
+
+        }
+    }
+
 }
